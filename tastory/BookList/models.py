16ac1f91a3e8c6992a441c3223ab3b94953b8501
Byun Jaewoo <qus7146@naver.com>
2022-07-06 16:22:10 +0900

    django DB, admin Add
    django +

--- tastory/BookList/models.py
+++ tastory/BookList/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BestBookList(models.Model):
+    title = models.TextField()         
+    author = models.TextField()    
+    content = models.TextField()
+    dt_created = models.DateField(auto_now=True)
+    
+    def __str__(self):                                   
+        return self.title
