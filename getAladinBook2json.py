9e0c3319e502c76c110c97c42536d89e46912f08
Byun Jaewoo <qus7146@naver.com>
2022-07-09 22:38:08 +0900

    Ver 7.9.1
    더미 데이터 100개 json에 추가

--- getAladinBook2json.py
+++ getAladinBook2json.py
@@ -7,26 +7,34 @@ endpg = 1000
 FinialBookDataList = []
 TitleList = []
 #BookDataDict = {'title':"", 'bookcover':"",'descript':"", 'author':""}
-key = '' #Input Key 
+key = 'ttbqus71461328001' #Input Key 
 print("\n\n\n\n\n\n")
-for A in range(2, 10):
-    BookDataDict = {'title':"", 'bookcover':"",'descript':"", 'author':""}
+for A in range(2, 103):
+    BookDataDict = {'title':"", 'img_url':"",'book_info':"", 'author':"", 'isbn':"", 'category':"", 'pubdate':"", 'publisher':""}
     url = f"http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey={key}&QueryType=ItemNewAll&MaxResults={A}&start={A}&SearchTarget=Book&output=xml&Version=20131101"
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
     TitleList = str(soup).split("</searchcategoryname>")[1]
-
+    #print(TitleList)
     title = TitleList.split("<title>",1)[1].split("</title>",1)[0]
     #print(title)
     author = TitleList.split("<author>",1)[1].split("</author>",1)[0]
     #print(author)
     description = TitleList.split("<description>",1)[1].split("</description>",1)[0]
-    print(description)
+    #print(description)
     logo = TitleList.split("<cover>",1)[1].split("</cover>",1)[0]
+    isbn = TitleList.split("<isbn>",1)[1].split("</isbn>",1)[0]
+    category = TitleList.split("<categoryname>",1)[1].split("</categoryname>",1)[0]
+    publisher = TitleList.split("<publisher>",1)[1].split("</publisher>",1)[0]
+    pubdate = TitleList.split("<pubdate>",1)[1].split("</pubdate>",1)[0]
     BookDataDict["title"] = title
-    BookDataDict["bookcover"] = logo
+    BookDataDict["img_url"] = logo
     BookDataDict["author"] = author
-    BookDataDict["descript"] = description
+    BookDataDict["book_info"] = description
+    BookDataDict["isbn"] = isbn
+    BookDataDict["category"] = category
+    BookDataDict["pubdate"] = pubdate
+    BookDataDict["publisher"] = publisher
     FinialBookDataList.append(BookDataDict)
     print("==========\n")
 
