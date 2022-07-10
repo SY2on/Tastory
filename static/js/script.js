cfdb4b42f429a9613d0e78a6cec8c2e30b4df17f
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-10 17:46:58 +0900

    [ CREATE ] 책 표시형식 변경 밒 swiperjs 연결

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var mySwiper = new Swiper('.bookcover', {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 24,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1280: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            720: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+            }
+        }
+    });
+    
+});
