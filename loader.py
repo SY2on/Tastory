f0d0ba6caf85be54c7d7d7d05681b42a31a2b0dc
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-10 10:20:55 +0900

    [ CREATE ] 상속 연동 확인

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def test():
-    return render_template("index.html")
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
