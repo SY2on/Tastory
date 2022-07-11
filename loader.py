9a98b69ceb6f755ed39625717f02bab520bca50b
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-11 18:49:32 +0900

    [ DESGIN ] 이달의 리뷰 칸 기초 디자인

--- loader.py
+++ loader.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 import json
 app = Flask(__name__)
 with open("testA.json") as f:
-	data = json.load(f)
+	bookdata = json.load(f)
+with open("user_review.json") as f:
+    review = json.load(f)
 @app.route("/")
 def test():
-    return render_template("test.html",data=data)
+    return render_template("test.html",bookdata=bookdata, review=review)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
