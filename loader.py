63608bb7f5f53b4d1610b4c1eb98d9012c912496
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-19 12:20:30 +0900

    backup

--- loader.py
+++ loader.py
@@ -5,9 +5,17 @@ with open("testA.json") as f:
 	bookdata = json.load(f)
 with open("user_review.json") as f:
     review = json.load(f)
+with open("userinfo.json") as f:
+    userinfo = json.load(f)
 @app.route("/")
 def test():
     return render_template("test.html",bookdata=bookdata, review=review)
+
+
+@app.route("/1")
+def aaa():
+    return render_template("anotheruser_libray.html",userinfo=userinfo)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
