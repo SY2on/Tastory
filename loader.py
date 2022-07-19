ab9414401ffb20fa440bd11622df298895866b48
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-19 20:14:46 +0900

    [ CREATE ] 다른사람서재 제작

--- loader.py
+++ loader.py
@@ -7,16 +7,14 @@ app = Flask(__name__)
 for DataName in os.listdir('./data'):
     with open("./data/"+DataName, encoding='UTF8') as f:
         globals()[os.path.splitext(DataName)[0]] = json.load(f)
-        
+
 @app.route("/")
 def test():
-    return render_template("test.html",bookdata=bookdata, review=review)
-
+    return render_template("test.html",bookdata=bookdata, review=user_review)
 
 @app.route("/1")
 def aaa():
-    return render_template("anotheruser_libray.html",userinfo=userinfo)
-
+    return render_template("anotheruser_libray.html",userinfo=userinfo,LibraryData=OtherUserLibraryInformation)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True) 
