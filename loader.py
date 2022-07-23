ab503ae6321c6f961e5f48ab74a3cc84334b6ded
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-23 11:26:58 +0900

    [ DESGIN ] 책 정보 디지인 1차

--- loader.py
+++ loader.py
@@ -17,7 +17,8 @@ def test():
     # return render_template("anotheruser_libray.html",UserInfo=UserInfo,LibraryData=OtherUserLibraryInformation) 
 
     # 책 정보
-    return render_template("Book_Info.html", UserInfo=UserInfo)
+    return render_template("Book_Info.html", UserInfo=UserInfo, BookInfo=BookInfo)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) 
+    
