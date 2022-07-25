9c2191c5b443ca578d335eba3af521bfd4d2f3b8
Seungwoo Ryu <ryuryu10@naver.com>
2022-07-26 01:07:45 +0900

    backup

--- loader.py
+++ loader.py
@@ -14,16 +14,16 @@ def test():
     # return render_template("Quest_View.html",BookData=bookdata, ReviewData=user_review, UserInfo=UserInfo) 
 
     # 다른사람 서재
-    # return render_template("anotheruser_libray.html",UserInfo=UserInfo,LibraryData=OtherUserLibraryInformation) 
+    #return render_template("anotheruser_libray.html",UserInfo=UserInfo,LibraryData=OtherUserLibraryInformation) 
 
     # 책 정보
-    #return render_template("Book_Info.html", UserInfo=UserInfo, BookInfo=BookInfo)
+    return render_template("Book_Info.html", UserInfo=UserInfo, BookInfo=BookInfo)
 
     # 로그인
     #return render_template("Login.html")
 
     # 다른사람 리뷰 화면
-    return render_template("AnotherUser_Review.html", UserInfo=UserInfo)
+    # return render_template("AnotherUser_Review.html", UserInfo=UserInfo, Datas=AnotherUser_Review)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) 
