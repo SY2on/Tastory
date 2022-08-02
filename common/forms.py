25c69efe4a49e99e3028f0d6538bffa179f95dc0
SY2on <sjk8w7@gmail.com>
2022-08-02 11:01:47 +0900

    username 삭제

--- common/forms.py
+++ common/forms.py
@@ -1,15 +1,18 @@
 from django import forms
-from .models import User
+from .models import User, Profile
 
 
 class SignupForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['email', 'username', 'nickname']
+        fields = ['nickname']
 
     def signup(self, request, user):
         user.email = self.cleaned_data['email']
-        user.username = self.cleaned_data['username']
         user.nickname = self.cleaned_data['nickname']
         user.save()
         return user
+
+
+class ProfileEditForm(forms.Form):
+    nickname = forms.CharField
--- common/models.py
+++ common/models.py
@@ -4,43 +4,43 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.mail import send_mail
 
 # Create your models here.
+
+
 class User(AbstractUser):
-  email = models.EmailField(
-    max_length=254,
-    unique=True
-    )
-  username_validator = UnicodeUsernameValidator()
-  username = models.CharField(
-    max_length=10,
-    help_text=("필수기입. 10자 이하. 문자, 숫자, @/./+/-/_ 만 가능."),
-    validators=[username_validator],
-    )
-  nickname = models.CharField(
-    max_length=15,
-    help_text=("필수기입. 15자 이하. 문자, 숫자, @/./+/-/_ 만 가능."),
-    validators=[username_validator]
-  )
+    user_id = models.BigAutoField(primary_key=True)
 
-  created_at = models.DateTimeField(auto_now_add =True)
-  updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(
+        max_length=254,
+        unique=True
+    )
+    nickname = models.CharField(
+        max_length=15,
+        help_text=("필수기입. 15자 이하. 문자, 숫자, @/./+/-/_ 만 가능."),
+    )
 
-  class Meta:
-    verbose_name = "user"
-    verbose_name_plural = "users"
-  def __str__(self):
-    return self.email
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.email
+
 
 class Profile(models.Model):
 
-  user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user_id = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
 
-  introduction = models.CharField(
-    max_length=100,
-    help_text=("100자 이하의 자기소개. 생략 가능합니다."),
-    blank=True,
-  )
-  profile_img = models.ImageField(
-    default='default.jpg',
-    upload_to='profile_pics',
-    null=True,
-  )
+    introduction = models.CharField(
+        max_length=100,
+        help_text=("100자 이하의 자기소개. 생략 가능합니다."),
+        blank=True,
+    )
+    profile_img = models.ImageField(
+        default='default.jpg',
+        upload_to='profile_pics',
+        null=True,
+    )
