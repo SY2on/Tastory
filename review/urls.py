3ef99cb751b8ceed1adb439bd89bbc9593331195
chaelink <lin1041840@gmail.com>
2022-08-06 00:00:04 +0900

    view upload

--- review/urls.py
+++ review/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('review/write/', views.write, name='review_write'),
+    path('review/<int:review_id>/', views.detail, name='review_detail'),
+    path('review/<int:review_id>/edit/', views.edit, name='review_edit'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Review 
+from .forms import ReviewForm
+
+def write(request) :
+    if request.method == 'POST' :
+        form = ReviewForm(request.POST)
+        new_review = form.save()
+        return redirect('page-read', review_id = new_review.id)
+    else :
+        form = ReviewForm()
+        return render(request, 'review_write.html', {'form':form} )
+
+
+def detail(request, Review_id):
+    object_detail = get_object_or_404(Review, id = Review_id)         #id or pk?
+    return render(request, 'review_detail.html', {'object_detail': object_detail})
+
+
+def edit(request, Review_id) :
+    review = Review.objects.get(id=Review_id)
+    if request.method == 'POST' :
+        review.title = request.POST['title']
+        review.content = request.POST['content']
+        review.status = request.POST['status']
+        review.save()
+        return redirect('review')
+    
+    else :
+        form = ReviewForm()
+        return render(request, 'review_edit.html', {'form':form} )
+
+
+
 
 # Create your views here.
