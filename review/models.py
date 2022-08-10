ef28cdc8a99b2357eb9edd10ec94ae055ed164fc
Byun Jaewoo <qus7146@naver.com>
2022-08-10 19:22:07 +0900

    Pull Request (Book)
    review -> common

--- review/models.py
+++ review/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Book(models.Model):
-    title = models.TextField()
-    img_url = models.TextField()
-    author = models.TextField()
-    book_info = models.TextField()
-    isbn = models.TextField(unique=True)
-    pubdate = models.DateField()
-    publisher = models.TextField()
-    category = models.TextField()
-
-    def __str__(self):
-        return self.title
--- review/urls.py
+++ review/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/all', views.all, name='book_all'),
-    path('book/<str:book_id>', views.read, name='book-detail')
 ]
